7938d142abf4d03974ee8dc79b05c2a1d5b3a6a6
G Ajay Kumar <g.ajjuajaykumar@gmail.com>
2026-01-28 19:48:05 +0530

    used lstm

--- main.py
+++ main.py
@@ -1,80 +1,72 @@
+import warnings
+# FutureWarning from the tf2onnx library
+warnings.filterwarnings("ignore", category=FutureWarning, module="keras.src.export.tf2onnx_lib")
+import os
+# Set the environment variables
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import keras
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 import joblib
 import numpy as np
 import re
-
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
 
 app = FastAPI(
     title="Sentiment Prediction API",
     description="FastAPI-based ML inference service"
 )
 
-lemmatizer = WordNetLemmatizer()
-stop_words = set(stopwords.words("english"))
-
-
+# Load model and tokenizer
 try:
-    model = joblib.load("logistic_regression_model.pk1")
-    vectorizer = joblib.load("tfidf_vectorizer.pk1")
-    print("Model and vectorizer loaded successfully")
+    model = keras.models.load_model("sentiment_analysis2.keras")
+    tokenizer = joblib.load("tokenizer.pkl")
+    print("Model and tokenizer loaded successfully")
 except Exception as e:
-    print(f"Failed to load model/vectorizer: {e}")
+    print(f"Failed to load model/tokenizer: {e}")
     model = None
-    vectorizer = None
+    tokenizer = None
 
-
+# Review
 class ReviewRequest(BaseModel):
     review: str
-
-
+# Sentiment & Probability
 class PredictionResponse(BaseModel):
     sentiment: str
-
-
-# ---------------------------
-# Text preprocessing
-# ---------------------------
-def preprocess_text(text: str) -> str:
+    probability: float
+    
+# text preprocessing 
+def preprocess_text(text: str) -> np.ndarray:
     text = text.lower()
     text = re.sub(r"<.*?>", "", text)
     text = re.sub(r"[^a-zA-Z\s]", "", text)
 
-    tokens = text.split()
-    cleaned_tokens = [
-        lemmatizer.lemmatize(word)
-        for word in tokens
-        if word not in stop_words
-    ]
+    MAX_LEN = 200
+    seq = tokenizer.texts_to_sequences([text])
+    padded = pad_sequences(seq, maxlen=MAX_LEN)
+    return padded
 
-    return " ".join(cleaned_tokens)
-
-
-# ---------------------------
-# Prediction endpoint
-# ---------------------------
 @app.post("/predict", response_model=PredictionResponse)
 def predict_sentiment(request: ReviewRequest):
-
-    if model is None or vectorizer is None:
+    # model and tokenizer
+    if model is None or tokenizer is None:
         raise HTTPException(status_code=500, detail="Model or vectorizer not loaded")
-
+    # if review not submitted
     if not request.review.strip():
         raise HTTPException(status_code=400, detail="Review text cannot be empty")
 
-    cleaned_text = preprocess_text(request.review)
-
-    try:
-        text_vector = vectorizer.transform([cleaned_text])
-        proba = model.predict_proba(text_vector)[0]
-        prob_positive = float(proba[1])
-        sentiment = "Positive" if prob_positive >= 0.35 else "Negative"
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Prediction failed: {str(e)}")
+    # handle text & 
+    seq_padded = preprocess_text(request.review)
+    prediction = model.predict(seq_padded)
+    prob_positive = float(prediction[0][0]) 
+    
+    sentiment = "Positive" if prob_positive >= 0.5 else "Negative"
 
     return {
         "sentiment": sentiment,
         "probability": round(prob_positive, 2)
     }
+
